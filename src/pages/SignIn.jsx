b8104e5f096886fefe926937d327eaf34b284ccc
Sr_Stroncio <razukmamo@gmail.com>
2025-04-14 08:47:13 +0100

    resposive del Sign in para laptop is tablet

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 import img from "../assets/login.png";
 
 function SignIn() {
+  //Logic para el login
+
   const [email, setEmail] = useState("usuario@ejemplo.com");
+
   const [password, setPassword] = useState("password");
   const { isDarkMode } = useUtilityMenu();
 
@@ -28,25 +31,28 @@ function SignIn() {
     }
   };
 
+  //Fin de la logica del login
+
   return (
     <>
       <div
         className={`${
           isDarkMode ? "bg-slate-950" : "bg-gray-100"
-        } h-[93vh] flex flex-col justify-center items-center`}
+        } h-[93vh] flex flex-col md:flex-row justify-center items-center`}
       >
-        <div className="flex justify-center items-center h-full">
-          <img className="w-60 h-60" src={img} alt="img" />
+        <div className="flex justify-center items-center h-1/3 md:h-1/2 md:w-1/2">
+          <img className="w-48 h-48 md:w-72 md:h-72" src={img} alt="img" />
         </div>
 
         <div
           className={`
             w-full max-w-md 
             ${isDarkMode ? "bg-slate-900" : "bg-white"}
-            rounded-t-3xl shadow-2xl p-8 space-y-6 mt-auto
+            rounded-t-3xl md:rounded-3xl shadow-2xl p-6 md:p-8 space-y-6
+            flex flex-col justify-center h-auto md:h-fit box-border overflow-hidden
           `}
         >
-          <form className="space-y-4" method="POST">
+          <form className="space-y-2 md:space-y-4" method="POST">
             <div>
               <label
                 htmlFor="username"
@@ -111,10 +117,10 @@ function SignIn() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-google"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-brand-google"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M20.945 11a9 9 0 1 1 -3.284 -5.997l-2.655 2.392a5.5 5.5 0 1 0 2.119 6.605h-4.125v-3h7.945z" />
@@ -129,10 +135,10 @@ function SignIn() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
@@ -146,7 +152,7 @@ function SignIn() {
                 height="24"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="icon icon-tabler icons-tabler-filled icon-tabler-brand-github"
+                className="icon icon-tabler icons-tabler-filled icon-tabler-brand-github"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z" />
@@ -156,7 +162,7 @@ function SignIn() {
           </div>
 
           <div
-            className={`text-center text-sm mt-4 ${
+            className={`text-center text-sm ${
               isDarkMode ? "text-white" : "text-gray-600"
             }`}
           >
