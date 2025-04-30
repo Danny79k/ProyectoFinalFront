c4d4c85cf3c98de89a235c0df845c3c91fb74725
Sr_Stroncio <razukmamo@gmail.com>
2025-04-30 10:41:24 +0100

    trabajando en el home

--- src/components/AsideL.jsx
+++ src/components/AsideL.jsx
@@ -1,31 +1,85 @@
 import Seguidos from "./Seguidos";
+import { IoPersonSharp } from "react-icons/io5";
+import { BsClipboard2PlusFill } from "react-icons/bs";
+import { RiUserFollowFill } from "react-icons/ri";
+import { useUtilityMenu } from "../store/useStore";
 
 function AsideL() {
+  const { isDarkMode, menuOpen, toggleTheme, toggleMenu } = useUtilityMenu();
+
   return (
-    <div className="asideL basis-[15%] bg-gray-100  border-gray-300">
-      <div className="asideL__container mt-8">
-        <div className="flex items-center justify-center mb-6">
+    <div className="asideL flex flex-col bg-gray-100 basis-[15%]">
+      <div className="asideL__container mt-2 flex flex-col flex-grow">
+        <div className="flex items-center mb-2 ml-2 p-1">
           <h3>Usuario</h3>
         </div>
 
-        <div className="buscador w-full mb-4 p-3 flex items-center justify-end">
+        <div className="buscador w-full mb-3 p-4 flex items-center justify-start">
           <input
             type="text"
-            placeholder="Buscar..."
-            className="block w-40 p-1 border rounded-md focus:outline-none"
+            placeholder="Search..."
+            className="block w-40 p-1 border rounded-md focus:outline-none placeholder:text-sm placeholder:text-gray-400"
           />
         </div>
 
-        <div className="options">
-          <div className="admin-options">
-            <h4>ADMIN</h4>
+        <div className="ml-2 w-40 flex flex-col items-start justify-start mb-4 space-y-1 text-gray-700">
+          <div className="admin-options w-full flex flex-row items-center p-2 hover:bg-gray-200">
+            <IoPersonSharp className="text-cyan-800" />
+            <p className="text-sm ml-2">My Posts</p>
           </div>
-
-          <div className="normal-options">
-            <h4>seguidos</h4>
-            <Seguidos />
+          <div className="admin-options w-full flex flex-row items-center p-2 hover:bg-gray-200">
+            <BsClipboard2PlusFill className="text-green-800" />
+            <p className="text-sm ml-2">New Posts</p>
           </div>
         </div>
+
+        <div className="ml-2 flex flex-col items-start justify-center mb-4 space-y-1 text-gray-700">
+          <div className="flex flex-row items-center p-2">
+            <RiUserFollowFill />
+            <p className="text-sm ml-2">Following</p>
+          </div>
+          <Seguidos />
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+
+      <div className="dark-mode-asideL mt-auto p-3 mb-2">
+        <button
+          onClick={toggleTheme}
+          className="flex items-center w-full px-4 py-3 text-gray-800 hover:bg-gray-200 transition-colors"
+        >
+          {isDarkMode ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="white"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+            </svg>
+          )}
+          <span className="ml-3 text-sm">Dark Mode</span>
+        </button>
       </div>
     </div>
   );
