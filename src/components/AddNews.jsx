eb38fc4fa29222ab1a3154f4503058d4e1e3f3f9
Danny79k <belloli.danny@gmail.com>
2025-05-14 13:21:56 +0300

    added real categories options

--- src/components/AddNews.jsx
+++ src/components/AddNews.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
 
 export const AddNews = () => {
+  const token = sessionStorage.getItem("token")
+
+  const { data, error, loading } = UseFetch('https://jeffrey.informaticamajada.es/api/categories', token);
+
+  if (loading) return <div className="flex justify-center items-center h-full">Cargando...</div>;
+  if (error) return <div className="flex justify-center items-center h-full">Error: {error}</div>;
+  const categories = data.data
+  console.log(categories)
+
+
   return (
     <div>
 
-        <form>
-            <input type="text" />
-            <textarea name="" id=""></textarea>
-            <input type="date" />
-            <input type="image" />
-            <select>
-                <option value="local">local</option>
-                <option value="regional">regional</option>
-                <option value="nacional">nacional</option>
-                <option value="internacional">internacional</option>
-            </select>
-            <input type="checkbox" value='urgent' />
-            <input type="checkbox" value='premium' />
-            <input type="number" />
-        </form>
+      <form>
+        <label htmlFor="">title</label>
+        <input type="text" />
+        <label htmlFor="">Content</label>
+        <textarea name="" id=""></textarea>
+        <label htmlFor="">date</label>
+        <input type="date" />
+        <label htmlFor="">image</label>
+        <input type="image" />
+        <label htmlFor="">type</label>
+        <select>
+          <option value="local">local</option>
+          <option value="regional">regional</option>
+          <option value="nacional">nacional</option>
+          <option value="internacional">internacional</option>
+        </select>
+        <label htmlFor="">urgent</label>
+        <input type="checkbox" value='urgent' />
+        <label htmlFor="">premium</label>
+        <input type="checkbox" value='premium' />
+        <label htmlFor="">category</label>
+        <select name="" id="">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.type}
+            </option>
+          ))}
+        </select>
+      </form>
     </div>
   )
 }
