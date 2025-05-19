0c331b5193ca798d739858dcf4422fb330dda539
Danny79k <belloli.danny@gmail.com>
2025-05-20 01:25:01 +0300

    refactor: improve rendering logic in Editors component and enhance readability

--- src/components/Editors.jsx
+++ src/components/Editors.jsx
@@ -9,29 +9,38 @@ export function Editors({ Editors }) {
 
   const { data, error, loading } = useFetch(`https://jeffrey.informaticamajada.es/api/users/${user.id}/following`, token);
 
-  if (loading) return <Loading tipo="Seguidos"/>;
-  if (error) return <Error error={error}/>;
-  
+  if (loading) return <Loading tipo="Seguidos" />;
+  if (error) return <Error error={error} />;
+
+  const followedIds = data.data?.map(item => item.redactor_id) || [];
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 w-full">
-      {data && Editors.map((item) => (
-        <div
-          key={item.id}
-          className="news rounded-xl shadow flex flex-col cursor-pointer"
-        >
-          <img src={item.img} alt="img" className="w-full h-40 object-cover" />
-          <div className="p-4 flex flex-row justify-around gap-2 flex-1">
-            <p className="text-sm font-medium truncate">{item.name}</p>
-            <button
-              onClick={() => handleFollow(item.id)}
-              className="bg-red-600 text-white py-1 px-3 rounded-xl text-sm w-fit hover:bg-red-700 transition"
-            >
-              Seguir
-            </button>
+      {Editors.map((item) => {
+        const isFollowed = followedIds.includes(item.id);
+        return (
+          <div
+            key={item.id}
+            className="news rounded-xl shadow flex flex-col cursor-pointer"
+          >
+            <img src={item.img} alt="img" className="w-full h-40 object-cover" />
+            <div className="p-4 flex flex-row justify-around gap-2 flex-1">
+              <p className="text-sm font-medium truncate">{item.name}</p>
+              {isFollowed && (<button
+                onClick={() => handleFollow(item.id)}
+                className="bg-red-600 text-white py-1 px-3 rounded-xl text-sm w-fit hover:bg-red-700 transition"
+              >
+                Seguir
+              </button>) }
+              {!isFollowed &&
+              (<p className="bg-green-600 text-white py-1 px-3 rounded-xl text-sm w-fit transition"
+              >
+                Ya lo sigues
+              </p>)}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   );
 }
