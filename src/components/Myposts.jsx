72fd30aeabc122cb6ed751f7d9cc5b1fa46e7c24
Sr_Stroncio <razukmamo@gmail.com>
2025-05-21 10:18:30 +0100

    Estetica de editors y de newsdetails fix

--- src/components/Myposts.jsx
+++ src/components/Myposts.jsx
@@ -1,39 +1,35 @@
 import UseFetch from "../hooks/UseFetch";
 import News from "./News";
 
-
 export function Myposts() {
+  const token = sessionStorage.getItem("token");
+  const currentUser = sessionStorage.getItem("user");
+  const currentUserParsed = JSON.parse(currentUser);
 
-    const token = sessionStorage.getItem("token");
-    const currentUser = sessionStorage.getItem("user");
-    const currentUserParsed = JSON.parse(currentUser);
-    
-    const { data, error, loading } = UseFetch(
-        `https://jeffrey.informaticamajada.es/api/users/${currentUserParsed.id}/news`,
-        token
-    );
-    
-    if (loading)
-        return (
-        <div className="flex justify-center items-center h-full text-gray-500">
-            Cargando...
-        </div>
-        );
-    
-    if (error)
-        return (
-        <div className="flex justify-center items-center h-full text-gray-500">
-            Error al cargar los datos
-        </div>
-        );
-    
+  const { data, error, loading } = UseFetch(
+    `https://jeffrey.informaticamajada.es/api/users/${currentUserParsed.id}/news`,
+    token
+  );
+
+  if (loading)
     return (
-        <div className="myposts flex flex-col w-full h-full p-4">
-        <h1 className="text-xl font-bold mb-4">Mis Publicaciones</h1>
-        <News noticias={data.data} />
-        </div>
+      <div className="flex justify-center items-center h-full text-gray-500">
+        Cargando...
+      </div>
     );
 
+  if (error)
+    return (
+      <div className="flex justify-center items-center h-full text-gray-500">
+        Error al cargar los datos
+      </div>
+    );
+
+  return (
+    <div className="home-content flex flex-row p-2 mt-4 h-[85vh] overflow-hidden overflow-y-auto">
+      <News noticias={data.data} />
+    </div>
+  );
 }
 
 export default Myposts;
